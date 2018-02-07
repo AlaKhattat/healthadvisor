cc7f0bd8cd5aa018c3e3ff0f227ce3b1ae7d50d9
khattout <khattout@Khattout-PC>
2018-02-07 18:31:30 +0100

    new commit

--- src/com/healthadvisor/impl/service/GestionUtilisateur.java
+++ src/com/healthadvisor/impl/service/GestionUtilisateur.java
@@ -5,20 +5,54 @@
  */
 package com.healthadvisor.impl.service;
 
+import com.healthadvisor.database.MyDB;
 import com.healthadvisor.entities.Utilisateur;
+import com.heathadvisor.service.IGestionMedecin;
 import com.heathadvisor.service.IGestionUtilisateur;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author khattout
  */
 public class GestionUtilisateur implements IGestionUtilisateur{
+ MyDB database;
 
+    public GestionUtilisateur() {
+        database = MyDB.getInstance();
+    }
     @Override
     public void AjouterUtilisateur(Utilisateur utilisateur) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+        try {
+            Statement stm =database.getConnexion().createStatement();
+            String sql="Insert into utilisateur (cin,nom,prenom,email,date_naiss,sexe,pays,ville) "+" values (?,?,?,?,?,?,?,?)";
+            PreparedStatement preparedStmt = database.getConnexion().prepareStatement(sql);
+            preparedStmt.setString(1,utilisateur.getCin());
+            preparedStmt.setString(2,utilisateur.getNom());
+            preparedStmt.setString(3,utilisateur.getPrenom());
+            preparedStmt.setString(4,utilisateur.getEmail());
+            java.util.Date utilStartDate = utilisateur.getDate_naiss();
+            java.sql.Date sqlStartDate = new java.sql.Date(utilStartDate.getTime());
+            preparedStmt.setDate(5, sqlStartDate );
+            preparedStmt.setString(6,utilisateur.getSexe());
+            preparedStmt.setString(7,utilisateur.getPays());
+            preparedStmt.setString(8,utilisateur.getVille());
+
+              preparedStmt.executeUpdate();
+
+            System.out.println("Insertion avec succes");
+           // stm.executeQuery(sql);
+        } catch (SQLException ex) {
+            Logger.getLogger(IGestionUtilisateur.class.getName()).log(Level.SEVERE, null, ex);
+        }       }
 
     @Override
     public void ModifierUtilisateur(Utilisateur utilisateur) {
@@ -27,17 +61,53 @@ public class GestionUtilisateur implements IGestionUtilisateur{
 
     @Override
     public void SupprimerUtilisateurCin(String cin) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            Statement stm =database.getConnexion().createStatement();
+            String sql="Delete from utilisateur where cin='"+cin+"'" ;
+            stm.executeUpdate(sql);
+            System.out.println("suppression avec succes");
+           // stm.executeQuery(sql);
+        } catch (SQLException ex) {
+            Logger.getLogger(IGestionUtilisateur.class.getName()).log(Level.SEVERE, null, ex);
+        }    
     }
 
     @Override
     public List<Utilisateur> ListUtilisateurs() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+           ArrayList<Utilisateur> listutil= new ArrayList<>();
+        try {
+            Statement stm =database.getConnexion().createStatement();
+            String sql="select * from utilisateur" ;
+            ResultSet rs = stm.executeQuery(sql);
+            
+            while(rs.next()){
+                Utilisateur util= new Utilisateur(rs.getString(0),rs.getString(1),rs.getString(2),rs.getString(3),rs.getDate(4), rs.getString(5), rs.getString(6), rs.getString(7));
+                listutil.add(util);
+            }
+            
+            System.out.println("Recuperation avec succes");
+           // stm.executeQuery(sql);
+        } catch (SQLException ex) {
+            Logger.getLogger(IGestionMedecin.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return listutil;     }
 
     @Override
     public Utilisateur AfficherUtilisateurCin(String cin) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Utilisateur util =null;
+        try {
+            Statement stm =database.getConnexion().createStatement();
+            String sql="select * from utilisateur where cin='"+cin+"'" ;
+            ResultSet rs = stm.executeQuery(sql);
+            
+            util= new Utilisateur(rs.getString(0),rs.getString(1),rs.getString(2),rs.getString(3),rs.getDate(4), rs.getString(5), rs.getString(6), rs.getString(7));
+            
+            System.out.println("Recuperation avec succes");
+           // stm.executeQuery(sql);
+        } catch (SQLException ex) {
+            Logger.getLogger(IGestionMedecin.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return util;    
     }
     
 }
