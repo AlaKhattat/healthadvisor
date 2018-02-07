33da863efb062d51ecfaf40fb747d8782b75f3e4
khattout <khattout@Khattout-PC>
2018-02-07 16:02:25 +0100

    new commit

--- src/com/heathadvisor/service/IGestionUtilisateur.java
+++ src/com/heathadvisor/service/IGestionUtilisateur.java
@@ -3,15 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.healthadvisor.entities;
+package com.heathadvisor.service;
 
 /**
  *
  * @author khattout
  */
-public class Utilisateur {
-    
-   
-    
+public interface IGestionUtilisateur {
     
 }
